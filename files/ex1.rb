714860606d2f6946ef7d8e433c84b6e18940e2b8
Ryan <ryan.geo@gmail.com>
2015-02-21 14:47:52 -0800

    Add exercises from Files chapter

---
+++ files/ex1.rb
@@ -0,0 +1,8 @@
+# ex1.rb
+
+simple = File.read("simple_file.txt")
+original = File.new("original_file.txt", "w+")
+File.open(original, "a") do |file|
+  file.puts simple
+end
+File.read(original)
---
+++ files/ex2.rb
@@ -0,0 +1,6 @@
+# ex2.rb
+
+d = Dir.new(".")
+while file = d.read do
+  puts "#{file} has extension .txt" if File.extname(file) == ".txt"
+end
